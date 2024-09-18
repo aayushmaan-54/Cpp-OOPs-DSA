3f61a17430cb11e579d44a758682d41d3a63eb95
Aayushmaan Soni <aayushmaan.soni54@gmail.com>
2024-09-19 01:24:35 +0530

    finished merge sort and added quick sort(incomplete)

--- 3. DSA (Basics)/3. Sorting/3-mergeSort.cpp
+++ 3. DSA (Basics)/3. Sorting/3-mergeSort.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int mergeTwoSortedArr() {
+void mergeTwoSortedArr() {
   int a[] = {10, 15, 20}, a_size = 3;
   int b[] = {5, 6, 6, 15}, b_size = 4;
   int i = 0, j = 0;
 
+  cout << "Merge 2 Sorted array in sorted way: ";
   while(i < a_size && j < b_size) {
     if(a[i] <= b[j]) {
       cout << a[i++] << " ";
@@ -21,15 +22,16 @@ int mergeTwoSortedArr() {
   while(j < b_size) {
     cout << b[j++] << " ";
   }
+  cout << endl;
 }
 
-int mergeSortedSubarrays() { // Here we take a single array with three points namely, the lower, the middle and the highest point. The elements from the lower to the middle are sorted and the elements from the (middle+1) to the higher are sorted. The task is to merge these two sorted parts into one.
-  int arr[] = {10, 20, 30, 15, 25, 35, 50, 60}; // here just focus on sorting [30, 15, 25], [35, 50]
-  int low = 2, mid = 4, high = 6;
+void mergeSortedSubarrays(int arr[], int low, int mid, int high) {
+  // Here we take a single array with three points namely, the lower, the middle and the highest point. The elements from the lower to the middle are sorted and the elements from the (middle+1) to the higher are sorted. The task is to merge these two sorted parts into one.
+  //  int arr[] = {10, 20, 15, 25, 30, 15, 50, 60}; Sorted subarrays: [15, 25, 30] and [15, 50]
     
   int n1 = mid - low + 1;
   int n2 = high - mid;
-  int left[n1], right[n2];
+  vector<int> left(n1), right(n2);
     
   for (int i = 0; i < n1; i++) {
     left[i] = arr[low + i];
@@ -43,61 +45,68 @@ int mergeSortedSubarrays() { // Here we take a single array with three points na
     
   while (leftIndex < n1 && rightIndex < n2) {
     if (left[leftIndex] <= right[rightIndex]) {
-      arr[k] = left[leftIndex];
-      leftIndex++;
-      k++;
+      arr[k++] = left[leftIndex++];
     } else {
-      arr[k] = right[rightIndex];
-      rightIndex++;
-      k++;
+      arr[k++] = right[rightIndex++];
     }
   }
     
   while (leftIndex < n1) {
-    arr[k] = left[leftIndex];
-    leftIndex++;
-    k++;
+    arr[k++] = left[leftIndex++];
   }
     
   while (rightIndex < n2) {
-    arr[k] = right[rightIndex];
-    rightIndex++;
-    k++;
+    arr[k++] = right[rightIndex++];
   }
-    
-  for (int i = 0; i < 8; i++) {
-    cout << arr[i] << " ";
+}
+
+void MergeSort(vector<int> &arr, int l, int r) {
+  if(r > l) { // atleast 2 ele should be in arr
+    int m = l + (r - l) / 2; // same as l + r / 2 but used this to avoid overflow for large values
+    MergeSort(arr, l, m); // sorts left half
+    MergeSort(arr, m + 1, r); // sorts right half
+    mergeSortedSubarrays(&arr[0], l, m, r); // merge subarray in sorted manner
   }
 }
 
 int main() {
-  // int size;
-  // cout << "Enter Size of Array: ";
-  // cin >> size;
+  mergeTwoSortedArr();
 
-  // vector<int> arr(size);
-  // for(int i = 0; i < arr.size(); ++i) {
-  //   cout << "Enter Array Element " << i + 1 << " : ";
-  //   cin >> arr[i];
-  // }
+  int arr[] = {10, 20, 15, 25, 30, 15, 50, 60};
+  mergeSortedSubarrays(arr, 2, 4, 6);
+  
+  cout << "Merge Sorted Sub Array: ";
+  for (int i = 0; i < 8; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
 
-  // cout << "Before Merge Sort: ";
-  // for(int i = 0; i < arr.size(); ++i) {
-  //   cout << arr[i] << " ";
-  // }
+  int size;
+  cout << "Enter Size of Array: ";
+  cin >> size;
 
-  // mergeTwoSortedArr();
-  mergeSortedSubarrays();
+  vector<int> vec(size);
+  for(int i = 0; i < vec.size(); ++i) {
+    cout << "Enter Array Element " << i + 1 << " : ";
+    cin >> vec[i];
+  }
 
-  // cout << "\nAfter Merge Sort: ";
-  // for(int i = 0; i < arr.size(); ++i) {
-  //   cout << arr[i] << " ";
-  // }
+  cout << "Before Merge Sort: ";
+  for(int i = 0; i < vec.size(); ++i) {
+    cout << vec[i] << " ";
+  }
+  cout << endl;
+
+  MergeSort(vec, 0, vec.size() - 1);
+
+  cout << "After Merge Sort: ";
+  for(int i = 0; i < vec.size(); ++i) {
+    cout << vec[i] << " ";
+  }
+  cout << endl;
 
   return 0;
 }
-
-
 
 /*
 
---
+++ 3. DSA (Basics)/3. Sorting/4-quickSort.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int naivePartition(int *arr, int size, int partitionEleIndex) {
+  
+}
+
+int lomutoPartition() {
+
+}
+
+int hoarePartition() {
+
+}
+
+int main() {
+  int arr[] = {3, 8, 6, 12, 10, 7, 7};
+  naivePartition(arr, 7, 5);
+
+  return 0;
+}
+
+/*
+
+* In the partitioning phase of QuickSort, the partition element is placed in its correct position with all elements less than or equal to it on its left and all elements greater on its right, while elements equal to the partition element are moved to the front.
+
+*/
